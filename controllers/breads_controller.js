194a32629dc74e433c678f73b38a74506f0e96b3
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-07-06 21:33:41 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,14 +3,20 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
+
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+      {
+        breads: Bread,
+        title: 'Index Page'
+      }
+    )
 })
 
-module.exports = breads
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
   })
-  
+
+  module.exports = breads
